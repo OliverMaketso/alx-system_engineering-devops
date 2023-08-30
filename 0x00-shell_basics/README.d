a6825dcbd940fd09357a875977762060376b1623
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-08-30 13:09:23 +0300

    Create README.d

---
+++ 0x00-shell_basics/README.d
@@ -0,0 +1,15 @@
+# Ubuntu Bash Scripting Repository
+
+This repository contains a collection of simple and concise Bash scripts designed to perform various tasks on Ubuntu 20.04 LTS. Each script adheres to specific requirements and conventions outlined below.
+
+## Scripts
+
+### 1. Print Current Directory
+
+Script: [print_current_directory.sh](print_current_directory.sh)
+
+This script prints the absolute path of the current working directory using the `pwd` command.
+
+To run the script:
+```bash
+./print_current_directory.sh
